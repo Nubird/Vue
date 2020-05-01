d7bb03d2dd39a0dcda573c15813ce3898905edbc
yuanyimin <1499405446@qq.com>
2020-05-01 21:05:50 +0800

    project init

--- travel/src/main.js
+++ travel/src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
+  // 等价于{App:App},键和值相同时，可以只写一个
   template: '<App/>'
 })
+
+// 路由就是根据网址的不同，返回不同的内容给用户
---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+Hello World
+</div>
+
+</template>
+
+<script type="text/javascript">
+export default{
+  name: 'Home'
+}
+</script>
+
+<style type="text/css">
+</style>
