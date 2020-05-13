ba91bbb013115c252ceb533dbe06d40f20f550b9
yuanyimin <1499405446@qq.com>
2020-05-13 21:50:14 +0800

    city-ajax

--- travel/src/pages/city/components/Alphabet.vue
+++ travel/src/pages/city/components/Alphabet.vue
@@ -1,16 +1,18 @@
 <template>
 	<ul class="list">
-		<li class="item">A</li>
-		<li class="item">B</li>
-		<li class="item">C</li>
-		<li class="item">D</li>
-		<li class="item">E</li>
+		<li class="item" v-for="(item,key) in cities" :key="key">
+			{{key}}
+		</li>
+
 	</ul>
 </template>
 
 <script>
 export default{
-	name:'CityAlphabet'
+	name:'CityAlphabet',
+	props:{
+		cities:Object
+	}
 }
 </script>
 
@@ -21,7 +23,7 @@ export default{
 		flex-direction:column
 		justify-content:center		
 		position:absolute
-		top:1.58rem
+		top:2.68rem
 		right:0
 		bottom:0
 		width:.4rem
--- travel/src/pages/city/components/List.vue
+++ travel/src/pages/city/components/List.vue
@@ -7,80 +7,28 @@
 				<div class="button-wrapper">
 					<div class="button">广州</div>
 				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
 			</div>
 		</div>
 
 		<div class="area">
 			<div class="title border-topbottom">热门城市</div>
 			<div class="button-list">
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
-				</div>
-				<div class="button-wrapper">
-					<div class="button">广州</div>
+				<div class="button-wrapper" v-for="item of hot" :key="item.id">
+					<div class="button">{{item.name}}</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="area">
-			<div class="title border-topbottom">A</div>
+		<div class="area" v-for="(item,key) of cities" :key="key">
+			<div class="title border-topbottom">{{key}}</div>
 			<div class="item-list">
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>	
-			</div>
-		</div>
-		<div class="area">
-			<div class="title border-topbottom">A</div>
-			<div class="item-list">
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>	
-			</div>
-		</div>
-		<div class="area">
-			<div class="title border-topbottom">A</div>
-			<div class="item-list">
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>	
-			</div>
-		</div>
-		<div class="area">
-			<div class="title border-topbottom">A</div>
-			<div class="item-list">
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>
-				<div class="item border-bottom">广州</div>	
+				<div
+				class="item border-bottom"
+				v-for="innerItem of item"
+				:key="innerItem.id"
+				>
+					{{innerItem.name}}
+				</div>
 			</div>
 		</div>
 	  </div>	
@@ -93,6 +41,10 @@ export default{
 	name:"CityList",
 	mounted(){
 		this.scroll=new Bscroll(this.$refs.wrapper)
+	},
+	props:{
+		hot:Array,
+		cities:Object
 	}
 }
 </script>
