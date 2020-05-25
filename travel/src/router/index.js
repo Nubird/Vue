e478f598ec209041931ac9442a35f396755d90d8
yuanyimin <1499405446@qq.com>
2020-05-25 08:34:21 +0800

    异步组件加载

--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -1,25 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
 export default new Router({
+  // 异步组件加载
   routes: [{
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home')
+      // component: Home
     },{
     	path: '/city',
-      	name: 'City',
-      	component: City
+    	name: 'City',
+      component: () => import('@/pages/city/City')
+      	// component: City
     },{
     	// 动态路由，跟随一个id
     	path:'/detail/:id',
     	name:'Detail',
-    	component:Detail
+      component: () => import('@/pages/detail/Detail')
+    	// component:Detail
     }],
     //页面切换时，始终显示在最顶部
     scrollBehavior (to, from, savedPosition) {
